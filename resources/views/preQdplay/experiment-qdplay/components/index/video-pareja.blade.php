8b9e37646da4c7b82a3825cdff266cd81f26e7ed
Lalo Medina <eulalio.09@icloud.com>
2022-07-22 08:32:16 -0500

    correccion de nombre de asesores, estaban al reves

--- resources/views/preQdplay/experiment-qdplay/components/index/video-pareja.blade.php
+++ resources/views/preQdplay/experiment-qdplay/components/index/video-pareja.blade.php
@@ -1,13 +1,13 @@
 <div class="row mb-5" style="margin-left: 5%; margin-right: 5%;">
     <div class="col-md-12 text-left">
         <h2 style="margin-bottom: -2%;">
-            <span class="text-secondary">2.</span>
-            <span class="text-white">¡Ya no rentes! Mejor invierte en tu patrimonio</span>
+            <span class="text-secondary">1.</span>
+            <span class="text-white">Administra el dinero en pareja sin conflictos</span>
         </h2>
         <hr>
         <p class="font-weight-normal text-white">
-            Aprende lo que debes evaluar antes de comprar tu primera casa, define metas
-            alcanzables de ahorro y conoce los beneficios de tener un inmueble.
+            Aprende a ordenar las finanzas en pareja, tomar decisiones financieras en
+            conjunto y un control de las finanzas transparente y claro.
         </p>
     </div>
 </div>
@@ -17,17 +17,15 @@
             <img src="{{ asset('index_files/experimento/finanzas-en-pareja.png')}}" alt="Finanzas en pareja" class="img-portada-video">
         </a>
     </div>
-    <div class="col-md-6">
+    <div class="col-sm6 col-md-6 col-lg-6">
         <h1 class="mb-5 text-left font-weight-bold title-underline-blue title-underline-b mt-3 text-white">
-            Guía para comprar<br>
-
-            tu primera casa
+            Finanzas en pareja
         </h1>
         <p class="text-white">
-            Ludivina Córdoba
+            Karen Vega
         </p>
         <img src="{{ asset('index_files/experimento/tiempo.png') }}" alt="Tiempo curso" width="5%">
-        <span class="text-white">&nbsp;&nbsp;3 horas</span>
+        <span class="text-white">&nbsp;&nbsp;3 horas 43 minutos</span>
     </div>
 </div>
 
--- resources/views/preQdplay/experiment-qdplay/components/index/video-renta.blade.php
+++ resources/views/preQdplay/experiment-qdplay/components/index/video-renta.blade.php
@@ -2,13 +2,13 @@
 <div class="row mb-5" style="margin-left: 5%; margin-right: 5%;">
     <div class="col-md-12 text-left">
         <h2 style="margin-bottom: -2%;">
-            <span class="text-secondary">1.</span>
-            <span class="text-white">Administra el dinero en pareja sin conflictos</span>
+            <span class="text-secondary">2.</span>
+            <span class="text-white">¡Ya no rentes! Mejor invierte en tu patrimonio</span>
         </h2>
         <hr>
         <p class="font-weight-normal text-white">
-            Aprende a ordenar las finanzas en pareja, tomar decisiones financieras en
-            conjunto y un control de las finanzas transparente y claro.
+            Aprende lo que debes evaluar antes de comprar tu primera casa, define metas
+            alcanzables de ahorro y conoce los beneficios de tener un inmueble.
         </p>
     </div>
 </div>
@@ -18,16 +18,19 @@
             <img src="{{ asset('index_files/experimento/guia-comprar-casa.png')}}" alt="Guia comprar casa" class="img-portada-video">
         </a>
     </div>
-    <div class="col-sm6 col-md-6 col-lg-6">
+    <div class="col-md-6">
         <h1 class="mb-5 text-left font-weight-bold title-underline-blue title-underline-b mt-3 text-white">
-            Finanzas en pareja
+            Guía para comprar<br>
+
+            tu primera casa
         </h1>
         <p class="text-white">
-            Karen Vega
+            Ludivina Córdoba
         </p>
         <img src="{{ asset('index_files/experimento/tiempo.png') }}" alt="Tiempo curso" width="5%">
-        <span class="text-white">&nbsp;&nbsp;3 horas 43 minutos</span>
+        <span class="text-white">&nbsp;&nbsp;3 horas</span>
     </div>
+
 </div>
 
 
