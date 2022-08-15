15fe9a68e4afa642564e06d16fe021189ecee05e
PC Vector Eumedina <lalo@queridodinero.com>
2022-08-15 10:21:21 -0500

    archivos iniciales

---
+++ database/migrations/2022_08_15_095057_create_reschedules_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateReschedulesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('reschedules', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('status')->nullable();
+            $table->integer('user_id')->nullable()->unsigned()->index();
+            $table->integer('advice_id')->nullable()->unsigned()->index();
+            $table->integer('type_user')->nullable();//1 = asesor || 0 = usuario asesorado
+            $table->text('description')->nullable();
+            $table->timestamps();
+            $table->softDeletes();
+
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('set null');
+            $table->foreign('advice_id')->references('id')->on('advice')->onDelete('set null');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('reschedules');
+    }
+}
