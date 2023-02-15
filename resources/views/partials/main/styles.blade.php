8974cf23742443c3012b92be54a1a642110830e0
Lalo Medina <eulalio.09@icloud.com>
2023-02-15 10:44:56 -0600

    actualice 2 archivos para que actualice la cache de los navegadores

--- resources/views/partials/main/styles.blade.php
+++ resources/views/partials/main/styles.blade.php
@@ -1,5 +1,6 @@
-<link href="{{ mix('css/app.css') }}" rel="stylesheet">
-<link href="{{ asset('css/custom.css') }}" rel="stylesheet">
+<!-- agregue version rand para actualizar la cache de los navegadores -->
+<link href="{{ mix('css/app.css') }}?v={{ (rand(1,500)) }}" rel="stylesheet">
+<link href="{{ asset('css/custom.css') }}?v={{ (rand(1,500)) }}" rel="stylesheet">
 <link href="https://cdn.lineicons.com/3.0/lineicons.css" rel="stylesheet">
 
 @stack('styles')
