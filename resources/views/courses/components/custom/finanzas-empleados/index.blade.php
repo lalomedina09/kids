1f6c52b5ca4567d24549da4d7d372cb4cd935d44
PC Vector Eumedina <lalo@queridodinero.com>
2023-04-12 15:05:22 -0600

    actualizacion fondos landing finanzas empleados

--- resources/views/courses/components/custom/finanzas-empleados/index.blade.php
+++ resources/views/courses/components/custom/finanzas-empleados/index.blade.php
@@ -2,8 +2,8 @@
 
 @include('courses.components.custom.finanzas-personales.banner')
 
-@include('courses.components.custom.finanzas-personales.about')
+@include('courses.components.custom.finanzas-empleados.about')
 
 @include('courses.components.custom.finanzas-empleados.benefits')
 
-@include('courses.components.custom.finanzas-personales.experience')
+@include('courses.components.custom.finanzas-empleados.experience')
