42550c85184c235679aed1d0e2fcff236e94af38
PC Vector Eumedina <lalo@queridodinero.com>
2023-04-18 15:09:26 -0600

    imagenes y particulas para la vista de herramientas

---
+++ resources/views/partials/profiles/tools.blade.php
@@ -0,0 +1,20 @@
+<div id="{{ str_slug(__('Tools')) }}" class="tab-pane">
+    {{--
+    aqui van las herrtamienasidasub
+    <!-- Archivo para  botones dentro de la seccion-->
+    @include('partials.profiles.components.btn-company')
+    <hr>
+    @include('partials.profiles.components.companies.form')
+    --}}
+    <div class="row">
+        <div class="col-md-12">
+            Herramientas
+        </div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4">
+
+        </div>
+    </div>
+</div>
