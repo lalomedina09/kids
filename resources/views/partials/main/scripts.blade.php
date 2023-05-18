7725241759f15979f9fee4bd151c2733be83f2c3
Lalo Medina <eulalio.09@icloud.com>
2023-05-18 13:27:54 -0600

    update en scripts

--- resources/views/partials/main/scripts.blade.php
+++ resources/views/partials/main/scripts.blade.php
@@ -1,6 +1,6 @@
 @php $urlQdplayCompany = Route::currentRouteName();@endphp
 @if (app()->environment() === 'production')
-    @if ($urlQdplayCompany != "register.qdplay.show")
+    {{--@if ($urlQdplayCompany != "register.qdplay.show")--
         <script async src="https://www.googletagmanager.com/gtag/js?id={{ config('services.google.analyticsKey') ?: 'UA-XXXXX-Y' }}"></script>
         <script type="text/javascript">
             window.dataLayer = window.dataLayer || [];
@@ -11,61 +11,20 @@
             gtag('config', "{{ config('services.google.analyticsKey') ?: 'UA-XXXXX-Y' }}");
             gtag('config', "{{ config('services.google.adsKey') ?: 'AW-NNNNNNNNN' }}");
         </script>
-    @endif
-    @if(false)<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/4887802.js"></script>@endif
+    {{--@endif--}}
+    <!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-S7ZQGNNHEF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
 
-    <script>
-        !function(f,b,e,v,n,t,s)
-        {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
-        n.callMethod.apply(n,arguments):n.queue.push(arguments)};
-        if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
-        n.queue=[];t=b.createElement(e);t.async=!0;
-        t.src=v;s=b.getElementsByTagName(e)[0];
-        s.parentNode.insertBefore(t,s)}(window,document,'script',
-        'https://connect.facebook.net/en_US/fbevents.js');
-        fbq('init', "{{ config('services.facebook.pixelId') ?: '' }}");
-        fbq('track', 'PageView', 'Purchase');
-    </script>
-    <noscript>
-        <img height="1" width="1" style="display:none" src="https://www.facebook.com/tr?id={your-pixel-id-goes-here}&ev=PageView&noscript=1"/>
-    </noscript>
+  gtag('config', 'G-S7ZQGNNHEF');
+</script>
+@if(false)<script type="text/javascript" id="hs-script-loader" async defer src="//js.hs-scripts.com/4887802.js"></script>@endif
 
-    <div id="fb-root"></div>
-    <div class="fb-customerchat"
-        attribution=setup_tool
-        page_id="1494181534177770"
-        theme_color="#FF6161"
-        logged_in_greeting="Hola, ¿cómo podemos ayudarte?"
-        logged_out_greeting="Hola, ¿cómo podemos ayudarte?">
-    </div>
+    @include('partials.main.scripts.facebook')
 
-    <script type="text/javascript">
-        window.fbAsyncInit = function() {
-            FB.init({
-                xfbml            : true,
-                version          : 'v5.0'
-            });
-        };
-
-        (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = 'https://connect.facebook.net/es_LA/sdk/xfbml.customerchat.js';
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    </script>
-
-    <script type="text/javascript">
-        (function(h,o,t,j,a,r){
-            h.hj=h.hj||function(){(h.hj.q=h.hj.q||[]).push(arguments)};
-            h._hjSettings={hjid:{{ config('services.hotjar.hjid') }},hjsv:{{ config('services.hotjar.hjsv') }}};
-            a=o.getElementsByTagName('head')[0];
-            r=o.createElement('script');r.async=1;
-            r.src=t+h._hjSettings.hjid+j+h._hjSettings.hjsv;
-            a.appendChild(r);
-        })(window,document,'https://static.hotjar.com/c/hotjar-','.js?sv=');
-    </script>
 @else
     <script type="text/javascript">
         (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
@@ -80,17 +39,7 @@
     </script>
 @endif
 
-<!-- Google Analytics 4-->
-<!-- Google tag (gtag.js) -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-S7ZQGNNHEF"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
-
-  gtag('config', 'G-S7ZQGNNHEF');
-</script>
-<!-- End Google Analytics 4-->
+@include('partials.main.scripts.google')
 
 <input type="hidden" name="_token" id="token" value="{{ csrf_token()}}">
 <script type="text/javascript" src="{{ asset('i18n?v='.config('money.app.subversion')) }}"></script>
---
+++ resources/views/partials/main/scripts/google.blade.php
@@ -0,0 +1,11 @@
+<!-- Google Analytics 4-->
+<!-- Google tag (gtag.js) -->
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'G-S7ZQGNNHEF');
+</script>
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-S7ZQGNNHEF"></script>
+<!-- End Google Analytics 4-->
