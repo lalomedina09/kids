fe1d9893d35cec1c7213425cc51435773ad853f8
Lalo Medina <eulalio.09@icloud.com>
2023-05-18 16:06:45 -0600

    script de google ads

--- resources/views/partials/main/script-body.blade.php
+++ resources/views/partials/main/script-body.blade.php
@@ -11,3 +11,13 @@
 <noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-WMT5M2C"
 height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
 <!-- End Google Tag Manager (noscript) -->
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=AW-785479071"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'AW-785479071');
+</script>
