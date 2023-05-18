9604bc6d6f98c560e3b5417ce8f85089e23e816f
Lalo Medina <eulalio.09@icloud.com>
2023-05-18 14:02:52 -0600

    propiedad de google analytics g4

--- resources/views/partials/main/scripts/google.blade.php
+++ resources/views/partials/main/scripts/google.blade.php
@@ -11,3 +11,13 @@
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-S7ZQGNNHEF"></script>
 --}}
 <!-- End Google Analytics 4-->
+
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-S7ZQGNNHEF"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-S7ZQGNNHEF');
+</script>
