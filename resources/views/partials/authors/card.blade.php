611c161e39ee05d61b312c36c60a299b5e3d5d32
Eumedina PC Vector <eulalio.09@icloud.com>
2023-06-12 09:03:51 -0600

    correccion en card para foto de escritor todos de forma redonda

--- resources/views/partials/authors/card.blade.php
+++ resources/views/partials/authors/card.blade.php
@@ -1,6 +1,6 @@
 <div class="col-xl-3 col-lg-3 col-md-4 col-sm-4 col-12 text-center">
     <a href="{{ route('authors.show', [$author->profile_key]) }}" class="d-block">
-        <img src="{{ $author->present()->author_photo }}" class="image--article" alt="{{ $author->present()->fullname }}">
+        <img src="{{ $author->present()->author_photo }}" class="image--article" style="border-radius: 250px; width: 210px;" alt="{{ $author->present()->fullname }}">
     </a>
 
     <a href="{{ route('authors.show', [$author->profile_key]) }}"
