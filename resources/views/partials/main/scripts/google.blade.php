e5f46d2dc76b16dabaa9f271f694d51876cd960d
Lalo Medina <eulalio.09@icloud.com>
2023-07-05 10:10:40 -0600

    actualizacion de etiquetas de google

--- resources/views/partials/main/scripts/google.blade.php
+++ resources/views/partials/main/scripts/google.blade.php
@@ -1,19 +1,8 @@
-<!-- Google Analytics 4-->
-<!-- Google tag (gtag.js) -->
-{{--
-<script>
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'G-S7ZQGNNHEF');
-</script>
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-S7ZQGNNHEF"></script>
---}}
-<!-- End Google Analytics 4-->
-
-<!-- Este script se obtuvo de google analytics-->
-<!-- Google tag (gtag.js) -->
+<!-- Actualizado el 5 de julio de 2023-->
+<!--
+    Google tag (gtag.js)
+    Esta etiqueta va para Google Analytics
+-->
 <script async src="https://www.googletagmanager.com/gtag/js?id=G-S7ZQGNNHEF"></script>
 <script>
   window.dataLayer = window.dataLayer || [];
@@ -23,32 +12,14 @@
   gtag('config', 'G-S7ZQGNNHEF');
 </script>
 
-<!-- Este script de obtuvo de tagmamager.google.com-->
-<!-- Google Tag Manager -->
+<!-- Actualizado el 5 de julio de 2023--->
+<!--
+    Google Tag Manager
+    Esta etiqueta va para Google Tag Manager
+-->
 <script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
 })(window,document,'script','dataLayer','GTM-WMT5M2C');</script>
-<!-- End Google Tag Manager -->
-
-<!-- actualizado 19 de mayo de 2023 -->
-<!-- Google Tag Manager -->
-<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-NNLCK6J');</script>
-<!-- End Google Tag Manager -->
-
-
-<!-- actualizado 12 de unio de 2023 para landing: https://www.queridodinero.com/registro-qdplay-empresas-->
-<!-- Google Tag Manager -->
-<script>
-    (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-TRQP777');
-</script>
 <!-- End Google Tag Manager -->
