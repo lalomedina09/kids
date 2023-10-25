b83d21ed5a67468a1ae6329bcc614c22414fa88c
Eulalio Medina Barragan <36649331+lalomedina09@users.noreply.github.com>
2023-10-25 12:15:30 -0600

    export de usuarios de yuhu

---
+++ app/Exports/QdpCollaborationsYuhuExport.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Exports;
+
+use Illuminate\Contracts\View\View;
+use Maatwebsite\Excel\Concerns\FromView;
+
+class QdpCollaborationsYuhuExport implements FromView
+{
+
+    public function __construct(string $form, $data)
+    {
+        $this->form = $form;
+        $this->data = $data;
+    }
+
+    public function view(): View
+    {
+        return view('exports.collaborations.yuhu', [
+            'data' => $this->data
+        ]);
+    }
+}
