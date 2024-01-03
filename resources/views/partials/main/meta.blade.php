b72fef9baed0dcb07b0514a9dd3475dee47af701
Eumedina PC Vector <eulalio.09@icloud.com>
2024-01-03 11:37:05 -0600

    ajuste en archivo meta para cambiar modo de abrir php

--- resources/views/partials/main/meta.blade.php
+++ resources/views/partials/main/meta.blade.php
@@ -44,11 +44,11 @@
 <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
 <meta http-equiv="Pragma" content="no-cache">
 
-<?php
+@php
 
 header("Cache-Control: no-cache, must-revalidate"); // HTTP/1.1
 header("Expires: Sat, 1 Jul 2022 05:00:00 GMT"); // Fecha en el pasado
 
-?>
+@endphp
 
 @stack('meta')
