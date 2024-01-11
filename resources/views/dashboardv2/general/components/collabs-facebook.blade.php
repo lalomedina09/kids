863bec23b1ac161cd645ec48e883f89d2afed9ea
Eumedina PC Vector <eulalio.09@icloud.com>
2024-01-11 10:17:56 -0600

    ajuste en tiempo de respuesta de facebook

--- resources/views/dashboardv2/general/components/collabs-facebook.blade.php
+++ resources/views/dashboardv2/general/components/collabs-facebook.blade.php
@@ -1,8 +1,8 @@
 <div class="card">
     <div class="card-body widget-user">
         <div class="text-center mt-4 mb-3">
-            <h2 class="fw-normal text-primary display-4" data-plugin="counterup">
-                10
+            <h2 class="fw-normal text-primary display-4">
+                <span data-plugin="counterup">8</span> h <span data-plugin="counterup">10</span>m
             </h2>
             <h5>Promedio de respuesta de facebook</h5>
         </div>
