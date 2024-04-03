93fe4f3ccb546dfdcb7203d4bd32651a86450074
Eulalio Medina Barragan <36649331+lalomedina09@users.noreply.github.com>
2024-04-03 10:35:19 -0600

    texto en landing de talleres

--- resources/views/courses/components/custom/finanzas-empleados/index.blade.php
+++ resources/views/courses/components/custom/finanzas-empleados/index.blade.php
@@ -7,3 +7,16 @@
 @include('courses.components.custom.finanzas-empleados.benefits')
 
 @include('courses.components.custom.finanzas-empleados.experience')
+
+<section>
+    <div class="row" style="margin-top: -5px;">
+        <div class="col-md-12 col-12 " style="background-color: #262525;">
+            <h2 class="mb-5 pt-5 title-underline title-underline-b title-underline-center-black text-center">
+                <span class="font-weight-bold text-danger">Querido Dinero</span>
+				<span class="font-weight-bold text-white">
+					es la plataforma para aprender de dinero, ofrecemos cursos online, talleres, mentorías personalizadas y muchísimo contenido.
+				</span>
+            </h2>
+        </div>
+    </div>
+</section>
