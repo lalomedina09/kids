28be34680f257e57926ca398cfd12d288f0ad164
Eulalio Medina Barragan <36649331+lalomedina09@users.noreply.github.com>
2024-06-05 17:06:15 -0600

    modificacion en ruta redirecionada

--- app/Http/Controllers/TypeformWebhookController.php
+++ app/Http/Controllers/TypeformWebhookController.php
@@ -18,9 +18,14 @@ class TypeformWebhookController extends Controller
 
         // Extrae el email de la respuesta (asegúrate de ajustar esto según la estructura de tu respuesta)
         $email = $data['form_response']['answers'][0]['email'];
-        dd($email);
+
+
         // Construye la URL del segundo formulario con el email como parámetro
-        #$url = 'https://test.queridodinero.com/gestion-de-recursos?email=' . urlencode($email);
+        //este funciona en el server test
+        $url = 'https://test.queridodinero.com/indice-de-felicidad/gestion-de-recursos?email=' . urlencode($email);
+
+        //este funciona en el local
+        #$url = 'http://prod.querido-dinero.develop/indice-de-felicidad/gestion-de-recursos?email=' . urlencode($email);
 
         // Redirige al usuario al segundo formulario
         return redirect($url);
