c94bf964bd6be4710592483fdbf9dbc886c33727
Eulalio Medina Barragan <36649331+lalomedina09@users.noreply.github.com>
2024-06-06 09:08:59 -0600

    agregue clase de log

--- app/Http/Controllers/TypeformWebhookController.php
+++ app/Http/Controllers/TypeformWebhookController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Response;
 use Illuminate\Http\Request;
-
+use Illuminate\Support\Facades\Log;
 
 class TypeformWebhookController extends Controller
 {
