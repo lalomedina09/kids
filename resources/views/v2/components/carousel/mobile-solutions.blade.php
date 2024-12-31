6a8836b356e585895cb2510a5e7e759b9c7ed744
Eulalio Medina Barragan <36649331+lalomedina09@users.noreply.github.com>
2024-12-31 15:23:04 -0600

    ajustes links en servicios

--- resources/views/v2/components/carousel/mobile-solutions.blade.php
+++ resources/views/v2/components/carousel/mobile-solutions.blade.php
@@ -9,35 +9,43 @@
 
     <div class="carousel-inner">
         <div class="carousel-item active">
-            <div class="frame-4">
-                <img class="untitled-artwork img-fluid img-solution" style="height:100%;"
-                    src="{{ asset('version-2/images/imgsoluciones/untitled-artwork-1-1.gif') }}" />
-                <div class="agencia-creativa-de text-center">Agencia creativa<br />de nicho financiero</div>
-                <div class="arrow-right"></div>
-            </div>
+            <a href="{{ route('agency') }}">
+                <div class="frame-4">
+                    <img class="untitled-artwork img-fluid img-solution" style="height:100%;"
+                        src="{{ asset('version-2/images/imgsoluciones/untitled-artwork-1-1.gif') }}" />
+                    <div class="agencia-creativa-de text-center">Agencia creativa<br />de nicho financiero</div>
+                    <div class="arrow-right"></div>
+                </div>
+            </a>
         </div>
         <div class="carousel-item">
-            <div class="frame-5">
-                <img class="rompecabezas img-fluid img-solution" style="height:100%;"
-                    src="{{ asset('version-2/images/imgsoluciones/rompecabezas-2.gif') }}" />
-                <p class="consultor-a-de text-center">Bienestar financiero<br />en las empresas</p>
-            </div>
+            <a href="{{ route('consulting') }}">
+                <div class="frame-5">
+                    <img class="rompecabezas img-fluid img-solution" style="height:100%;"
+                        src="{{ asset('version-2/images/imgsoluciones/rompecabezas-2.gif') }}" />
+                    <p class="consultor-a-de text-center">Bienestar financiero<br />en las empresas</p>
+                </div>
+            </a>
         </div>
         <div class="carousel-item">
-            <div class="frame-6">
-                <img class="editorial img-fluid img-solution" style="height:100%;"
-                    src="{{ asset('version-2/images/imgsoluciones/editorial-1.gif') }}" />
-                <div class="contenidos text-center">Contenidos de<br />Finanzas Personales</div>
-                <div class="arrow-right-2"></div>
-            </div>
+            <a href="{{ route('blog') }}">
+                <div class="frame-6">
+                    <img class="editorial img-fluid img-solution" style="height:100%;"
+                        src="{{ asset('version-2/images/imgsoluciones/editorial-1.gif') }}" />
+                    <div class="contenidos text-center">Contenidos de<br />Finanzas Personales</div>
+                    <div class="arrow-right-2"></div>
+                </div>
+            </a>
         </div>
         <div class="carousel-item">
-            <div class="frame-7">
-                <img class="computadora img-fluid img-solution" style="height:100%;"
-                    src="{{ asset('version-2/images/imgsoluciones/computadora-1.gif') }}" />
-                <p class="academia-online text-center">Academia online<br />learning a la medida</p>
-                <div class="arrow-right-3"></div>
-            </div>
+            <a href="{{ route('qdplay') }}">
+                <div class="frame-7">
+                    <img class="computadora img-fluid img-solution" style="height:100%;"
+                        src="{{ asset('version-2/images/imgsoluciones/computadora-1.gif') }}" />
+                    <p class="academia-online text-center">Academia online<br />learning a la medida</p>
+                    <div class="arrow-right-3"></div>
+                </div>
+            </a>
         </div>
     </div>
 
--- resources/views/v2/home/index/services.blade.php
+++ resources/views/v2/home/index/services.blade.php
@@ -3,38 +3,46 @@
     <div class="row justify-content-center align-items-stretch servicio-lr">
         <!-- Item 1 -->
         <div class="col-md-2 col-sm-4 col-6 mb-4 d-flex">
-            <div class="frame-8 service-1 d-flex flex-column justify-content-between">
-            <div class="text-wrapper ">Blog</div>
+            <a href="{{ route('blog') }}">
+                <div class="frame-8 service-1 d-flex flex-column justify-content-between">
+                <div class="text-wrapper ">Blog</div>
 
-                <img class="img-2 img-2-service" src="{{ asset('version-2/images/imgservices/untitled-artwork-6-1.gif') }}" />
-                <div class="subtext-wrapper">Lee nuestros artículos</div>
-            </div>
+                    <img class="img-2 img-2-service" src="{{ asset('version-2/images/imgservices/untitled-artwork-6-1.gif') }}" />
+                    <div class="subtext-wrapper">Lee nuestros artículos</div>
+                </div>
+            </a>
         </div>
         <!-- Item 2 -->
         <div class="col-md-2 col-sm-4 col-6 mb-4 d-flex">
-            <div class="frame-8 service-2 d-flex flex-column justify-content-between">
-            <div class="text-wrapper ">Cursos online</div>
-                <img class="img-2 img-2-service" src="{{ asset('version-2/images/imgservices/untitled-artwork-8-1.gif') }}" style="margin-bottom: 1rem; margin-top: auto;">
-                <div class="subtext-wrapper">Aprende con expertos</div>
-            </div>
+            <a href="{{ url('qdplay') }}">
+                <div class="frame-8 service-2 d-flex flex-column justify-content-between">
+                <div class="text-wrapper ">Cursos online</div>
+                    <img class="img-2 img-2-service" src="{{ asset('version-2/images/imgservices/untitled-artwork-8-1.gif') }}" style="margin-bottom: 1rem; margin-top: auto;">
+                    <div class="subtext-wrapper">Aprende con expertos</div>
+                </div>
+            </a>
         </div>
 
         <!-- Item 3 -->
         <div class="col-md-2 col-sm-4 col-6 mb-4 d-flex">
-            <div class="frame-8 service-3 d-flex flex-column justify-content-between">
-            <div class="text-wrapper ">Newsletter</div>
-                <img class="img-2-service" src="{{ asset('version-2/images/imgservices/nesletter-1.gif') }}" />
-                <div class="subtext-wrapper">Recibe nuevas noticias</div>
-            </div>
+            <a href="https://queridodinero.myflodesk.com/comunidad">
+                <div class="frame-8 service-3 d-flex flex-column justify-content-between">
+                <div class="text-wrapper ">Newsletter</div>
+                    <img class="img-2-service" src="{{ asset('version-2/images/imgservices/nesletter-1.gif') }}" />
+                    <div class="subtext-wrapper">Recibe nuevas noticias</div>
+                </div>
+            </a>
         </div>
 
         <!-- Item 4 -->
         <div class="col-md-2 col-sm-4 col-6 mb-4 d-flex">
-            <div class="frame-8 service-4 d-flex flex-column justify-content-between">
-            <div class="text-wrapper ">Redes sociales</div>
-                <img class="img-2-service" src="{{ asset('version-2/images/imgservices/untitled-artwork-10-1.gif') }}" style="margin-top:2px;" />
-                <div class="subtext-wrapper">Ve nuestras publicaciones</div>
-            </div>
+            <a href="https://www.instagram.com/querido_dinero">
+                <div class="frame-8 service-4 d-flex flex-column justify-content-between">
+                <div class="text-wrapper ">Redes sociales</div>
+                    <img class="img-2-service" src="{{ asset('version-2/images/imgservices/untitled-artwork-10-1.gif') }}" style="margin-top:2px;" />
+                    <div class="subtext-wrapper">Ve nuestras publicaciones</div>
+                </div>
+            </a>
         </div>
 
         <!-- Item 5 -->
