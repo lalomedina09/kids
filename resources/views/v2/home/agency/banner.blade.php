ed21b622f9216f6ecd9fa63361e20c20a52539a3
CarlosZavalija <juan.garzazvlj@uanl.edu.mx>
2025-05-08 15:45:12 -0600

    Correcciones-QD

--- resources/views/v2/home/agency/banner.blade.php
+++ resources/views/v2/home/agency/banner.blade.php
@@ -6,7 +6,7 @@
         <!-- Contenido central -->
         <div class="row">
             <div class="col-12 col-md-12 text-center justify-content-center align-items-center h-100"
-                style="position: relative; z-index: 2; margin-bottom: 10rem;" >
+                style="position: relative; z-index: 2; margin-bottom: 10rem;">
                 <div class="position-relative d-flex justify-content-center align-items-center">
                     <img src="{{ asset('version-2/images/img_capa_1.png') }}" alt="Capa Imagen Izquierda"
                         class="img-fluid capaone_one pigeon-secondary-left">
@@ -37,4 +37,18 @@
                 <a href="{{ route('contact') }}" class="btn btn-dark mt-4 btn-learn-more">Contáctanos</a>
             </div>
         </div>
+    </div>
 </section>
+
+{{-- Aqui agregamos nueva imagen de fondo correccion manchas --}}
+
+<style>
+    .stackcapaone {
+    background-image: url('{{ asset("../images/services/background.png") }}'); /* Ruta de la imagen */
+    background-size: cover; /* Ajusta la imagen para cubrir todo el contenedor */
+    background-position: center; /* Centra la imagen */
+    background-repeat: no-repeat; /* Evita que se repita */
+    position: relative; /* Mantiene el posicionamiento relativo para los elementos hijos */
+    min-height: 100vh; /* Asegura que el contenedor tenga suficiente altura */
+}
+</style>
